54494048b0939e384492ab5f6f848f90bc76c4df
Tasie Ibiam <tasieibiam28@gmail.com>
2024-02-12 00:23:28 +0100

    file_storage.py corrected

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -72,18 +72,18 @@ class FileStorage:
                     "last_name": str},
             "State":
                 {"name": str},
-            "City":{"state_id": str, "name":str},
+            "City": {"state_id": str, "name": str},
             "Amenity": {"name": str},
             "Place":
                 {"city_id": str,
-                 "user_id": str,
-                 "name": str,
-                 "description": str,
-                 "number_rooms": int,
-                 "number_bathrooms": int,
-                 "max_guest": int,
-                 "price_by_night": int,
-                 "latitude": float,
-                 "longitude": float}
+                    "user_id": str,
+                    "name": str,
+                    "description": str,
+                    "number_rooms": int,
+                    "number_bathrooms": int,
+                    "max_guest": int,
+                    "price_by_night": int,
+                    "latitude": float,
+                    "longitude": float}
         }
         return attributes
